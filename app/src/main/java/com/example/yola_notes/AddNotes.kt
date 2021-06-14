fc06589a377f0dc96f66faa56730f2000e5b0a72
Khushi Sharma <khushi.sharma.2201@gmail.com>
2021-06-14 23:09:33 +0530

    Updated the Fragment for adding notes

--- app/src/main/java/com/example/yola_notes/AddNotes.kt
+++ app/src/main/java/com/example/yola_notes/AddNotes.kt
@@ -32,7 +32,8 @@ class AddNotes : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_notes, container, false)
+        val root = inflater.inflate(R.layout.fragment_add_notes, container, false)
+        return root
     }
 
     companion object {
